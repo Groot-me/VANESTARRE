50e975e5d337feb21d0b4c29ea05d64f0e27fef7
Bryan Battu <bryan.battu@etu.univ-amu.fr>
2021-01-13 16:35:35 +0100

    a

--- Test/Accueil.php
+++ Test/Accueil.php
@@ -1,7 +1,12 @@
-<?php
-require 'function.php';
-start_page('VANESTARRE - Connexion ou Inscription');
-?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>VANESTARRE - Connexion ou Inscription</title>
+    <link rel="stylesheet" href="Accueil.css">
+    <script src="script.js"></script>
+</head>
+<body>
     <h1>VANESTARRE</h1>
     <div class="form">
         <form id="form" action="" method="post">
@@ -15,110 +20,5 @@ start_page('VANESTARRE - Connexion ou Inscription');
         </form>
     </div>
 
-    <style>
-        body {
-            width: 100%;
-            height: 100%;
-            font-family: "Avenir", Helvetica, Arial, sans-serif;
-        }
-
-        html, body, div {
-            margin: 0;
-            padding: 0;
-            border: 0;
-            font: inherit;
-            font-size: 100%;
-            vertical-align: baseline;
-        }
-
-        #form {
-            position: absolute;
-            font-family: "Avenir";
-            color: rgb(0, 114, 163);
-            top: 35%;
-            left: 41.5%;
-            background-color: transparent;
-            padding: 30px 60px;
-            border: 1px solid #335D95;
-            border-radius: 10px;
-            box-shadow: 0 5px 15px -5px black;
-        }
-
-        input[type="submit"] {
-            width: 200px;
-            height: 40px;
-            border: 1px solid #335D95;
-            border-radius: 10px;
-            font-size: 14px;
-            font-weight: bold;
-            text-transform: uppercase;
-            cursor: pointer;
-            box-shadow: 0 5px 15px -5px #335D95;
-            margin-bottom: 10px;
-            font-style: italic;
-            color: white;
-            background-color: #335D95;
-        }
-
-        .form input[type="text"] {
-            margin-bottom: 10px;
-            padding-left: 20px;
-            width: 180px;
-            height: 40px;
-            border: 1px solid #335D95;
-            border-radius: 10px;
-            font-size: 14px;
-            color: #335D95;
-        }
-
-        .form input[type="password"] {
-            margin-bottom: 10px;
-            padding-left: 20px;
-            width: 180px;
-            height: 40px;
-            border: 1px solid #335D95;
-            border-radius: 10px;
-            font-size: 14px;
-            color: #335D95;
-        }
-
-        #forgot {
-            text-decoration: none;
-            font-size: 14px;
-            color: #335D95;
-            font-weight: bold;
-            position: absolute;
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-        .form button {
-            width: 200px;
-            height: 40px;
-            border: 1px solid green;
-            border-radius: 10px;
-            font-size: 14px;
-            font-weight: bold;
-            text-transform: uppercase;
-            cursor: pointer;
-            box-shadow: 0 5px 15px -5px green;
-            margin-top: 30px;
-            font-style: italic;
-            color: white;
-            background-color: green;
-        }
-
-        h1 {
-            font-family: 'Avenir';
-            color: #335D95;
-            font-size: 50px;
-            width: 333px;
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-    </style>
-
-<?php
-end_page();
-?>
+</body>
+</html>
