fddfd7a1c609a1198ea8f2301bdb5cde75b986d1
Bryan Battu <bryan.battu@etu.univ-amu.fr>
2021-01-13 15:56:35 +0100

    add html.php (function)

---
+++ Test/function.php
@@ -0,0 +1,14 @@
+// function start_page() et end_page()
+
+<?php
+function start_page($title)
+{
+    echo '<!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL;
+}
+
+function end_page()
+{
+    echo '</body></html>';
+}
+?>
