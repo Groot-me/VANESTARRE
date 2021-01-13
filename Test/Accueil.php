f6779b53da1832a18b63a1aa71ddb38de2dbd91d
Bryan Battu <bryan.battu@etu.univ-amu.fr>
2021-01-13 16:22:25 +0100

    a

--- Test/Accueil.php
+++ Test/Accueil.php
@@ -39,7 +39,7 @@ start_page('VANESTARRE - Connexion ou Inscription');
             left: 41.5%;
             background-color: transparent;
             padding: 30px 60px;
-            border: 1px solid hotpink;
+            border: 1px solid #335D95;
             border-radius: 10px;
             box-shadow: 0 5px 15px -5px black;
         }
@@ -47,17 +47,17 @@ start_page('VANESTARRE - Connexion ou Inscription');
         input[type="submit"] {
             width: 200px;
             height: 40px;
-            border: 1px solid hotpink;
+            border: 1px solid #335D95;
             border-radius: 10px;
             font-size: 14px;
             font-weight: bold;
             text-transform: uppercase;
             cursor: pointer;
-            box-shadow: 0 5px 15px -5px hotpink;
+            box-shadow: 0 5px 15px -5px #335D95;
             margin-bottom: 10px;
             font-style: italic;
             color: white;
-            background-color: hotpink;
+            background-color: #335D95;
         }
 
         .form input[type="text"] {
@@ -65,10 +65,10 @@ start_page('VANESTARRE - Connexion ou Inscription');
             padding-left: 20px;
             width: 180px;
             height: 40px;
-            border: 1px solid hotpink;
+            border: 1px solid #335D95;
             border-radius: 10px;
             font-size: 14px;
-            color: hotpink;
+            color: #335D95;
         }
 
         .form input[type="password"] {
@@ -76,16 +76,16 @@ start_page('VANESTARRE - Connexion ou Inscription');
             padding-left: 20px;
             width: 180px;
             height: 40px;
-            border: 1px solid hotpink;
+            border: 1px solid #335D95;
             border-radius: 10px;
             font-size: 14px;
-            color: hotpink;
+            color: #335D95;
         }
 
         #forgot {
             text-decoration: none;
             font-size: 14px;
-            color: hotpink;
+            color: #335D95;
             font-weight: bold;
             position: absolute;
             margin-left: auto;
@@ -95,22 +95,22 @@ start_page('VANESTARRE - Connexion ou Inscription');
         .form button {
             width: 200px;
             height: 40px;
-            border: 1px solid pink;
+            border: 1px solid green;
             border-radius: 10px;
             font-size: 14px;
             font-weight: bold;
             text-transform: uppercase;
             cursor: pointer;
-            box-shadow: 0 5px 15px -5px pink;
+            box-shadow: 0 5px 15px -5px green;
             margin-top: 30px;
             font-style: italic;
             color: white;
-            background-color: pink;
+            background-color: green;
         }
 
         h1 {
             font-family: 'Avenir';
-            color: pink;
+            color: #335D95;
             font-size: 50px;
             width: 333px;
             margin-left: auto;
